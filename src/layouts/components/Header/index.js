3a375896830f3bc668bc0c3bd76d61a943ccf72f
Viet Tran <viet.tranhoang92@gmail.com>
2023-02-09 15:21:28 +0700

    add search component & search result form

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -1,28 +1,33 @@
 import className from 'classnames/bind';
 import styles from './Header.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleQuestion, faEarthAsia, faKeyboard } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import images from '~/assets/images';
 import Menu from '../Menu';
 import Action from '../Action';
 import config from '~/configs';
+import Search from '~/layouts/components/SearchInput/SearchInput';
 
 const cx = className.bind(styles);
 
-function Header() {
+function Header({ children }) {
+    const [searchClick, setsearchClick] = useState(false);
+    const handleSearchClick = (e) => {
+        setsearchClick(!searchClick);
+    };
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={config.routes.home} className={cx('logo-link')}>
-                    <img src={images.logo} alt="test" />
-                </Link>
-                <div className="menu">
-                    <Menu />
+                <div className="logo">
+                    <Link to={config.routes.home} className={cx('logo-link')}>
+                        <img src={images.logo} alt="test" />
+                    </Link>
                 </div>
+                {!searchClick ? <Menu /> : <Search />}
+                {children}
                 <div className="action">
-                    <Action />
+                    <Action onSearchClick={handleSearchClick} />
                 </div>
             </div>
         </header>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,18 @@
 //Layout
-import { HeaderOnly } from '~/layouts';
+// import { HeaderOnly } from '~/layouts';
 
 import Product from '~/pages/Product/Product';
 import Home from '~/pages/Home/Home';
 import About from '~/pages/About/About';
 import config from '~/configs';
+import Search from '~/layouts/components/SearchInput/SearchInput';
 
 //Public routes without login
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.product, component: Product },
     { path: config.routes.about, component: About }, //, layout: HeaderOnly },
+    { path: config.routes.search, component: Search },
 ];
 //Routes required login
 const privateRoutes = [];
