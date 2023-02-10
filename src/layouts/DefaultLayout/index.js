bd914a0e5d3b1625284857a07c9f2a45bc1a0de6
Viet Tran <viet.tranhoang92@gmail.com>
2023-02-10 17:23:08 +0700

    add react boostrap and slide show

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,6 +1,7 @@
 import className from 'classnames/bind';
 
 import Header from '~/layouts/components/Header';
+import SlideShow from '../components/SlideShow';
 import styles from './DefaultLayout.module.scss';
 
 const cx = className.bind(styles);
@@ -9,6 +10,9 @@ function DefaultLayout({ children }) {
     return (
         <div className={cx('wrapper')}>
             <Header />
+            <div className={cx('slide-show')}>
+                <SlideShow />
+            </div>
             <div className={cx('container')}>
                 <div>{children}</div>
             </div>
