721d1b322c3c99c04453cef18745a09a186441dd
Viet Tran <viet.tranhoang92@gmail.com>
2023-02-27 13:42:54 +0700

    add page context and navtab menu

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,13 +1,35 @@
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
 import className from 'classnames/bind';
+import Carousel from 'react-bootstrap/Carousel';
+import { useState } from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
 import styles from './Product.module.scss';
+import Item from './component/Item';
 
 const cx = className.bind(styles);
-
+const settings = {
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+};
+console.log('re-render Product');
 function Product({ productId }) {
-    return <h2 style={{ height: '1000px' }}>Product Page: {productId}</h2>;
+    return (
+        <div className={cx('wapper')}>
+            <div className={cx('content')}>
+                <div className={cx('product-img')}>Img</div>
+                <div className={cx('product-title')}>
+                    <Slider {...settings}>
+                        <div className={cx('slick-track')}>
+                            <Item productId={productId} />
+                        </div>
+                    </Slider>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Product;
